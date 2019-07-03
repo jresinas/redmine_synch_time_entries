0c81845dd9d17f20fed0756b00a3c2addacafa9f
Hugo Mateo <hmateo@emergya.com>
2019-07-03 13:52:53 +0200

    Added manual synchronisation feature

--- app/controllers/synch_relations_controller.rb
+++ app/controllers/synch_relations_controller.rb
@@ -71,6 +71,90 @@ class SynchRelationsController < ApplicationController
         end
     end
 
+    # Borra las imputaciones sincronizados de los últimos meses
+    def clear_synch(offset_months = 3)
+        start_date = Date.today - offset_months.months
+        end_date = Date.today
+        deleted_time_entries_relations = SynchTimeEntryRelation.where("spent_on BETWEEN ? AND ?", start_date, end_date)
+        deleted_time_entries_relations.each do |te_relation|
+            if te_relation.time_entry.present?
+                te_relation.time_entry.delete
+            end
+            te_relation.delete
+        end
+    end
+
+    # Realiza la sincronización manual de las imputaciones de horas del origen con las del destino
+    def perform_manual_synch
+        offset_months = params[:number_of_months_ago].present? ? params[:number_of_months_ago].to_i : 3
+        if params[:clear_synch].present?
+            begin
+                clear_synch(offset_months)
+                flash[:notice] = l(:notice_successful_delete)
+            rescue
+                flash[:error] = l(:notice_unable_delete_time_entry)
+            end
+        else
+            begin
+                result_update = []
+                result_create = []
+
+                user_relations = SynchRelation.where(data_type: 'User')
+                issue_relations = SynchRelation.where(data_type: 'Issue')
+                project_relations = SynchRelation.where(data_type: 'Project')
+                project_relations_tree = SynchTimeEntries::Source.get_project_relations_tree(project_relations)
+                start_date = Date.today - offset_months.months
+                end_date = Date.today
+                time_entries = SynchTimeEntries::Source.get_time_entries(start_date, end_date)
+                time_entries_relations = SynchTimeEntryRelation.where("spent_on BETWEEN ? AND ?", start_date, end_date)
+
+                time_entries.each do |te|
+                    project_match = project_relations_tree.detect{|p| p[:id] == te[:project_id] or p[:descendants].include?(te[:project_id])}
+                    project_match_id = project_match.present? ? project_match[:id] : nil
+                    if (user_relation = user_relations.find_by(source_id: te[:user])) and ((issue_relation = issue_relations.find_by(source_id: te[:issue_id])) or (project_relation = project_relations.find_by(source_id: project_match_id)))
+                        # Obtenemos usuario de destino
+                        te[:user] = User.find(user_relation.target_id)
+
+                        # Obtenemos petición de destino (y proyecto si es relevante)
+                        if issue_relation.present?
+                            te[:issue_id] = issue_relation.target_id
+                            te[:project_id] = Issue.find(issue_relation.target_id).project_id
+                        elsif project_relation.present?
+                            te[:project_id] = project_relation.target_id
+                            subject = Setting.plugin_redmine_synch_time_entries['issue_subject']+" "+te[:spent_on].to_date.strftime("%m-%Y")
+                            user = Setting.plugin_redmine_synch_time_entries['issue_user']
+                            tracker = Setting.plugin_redmine_synch_time_entries['issue_tracker'] || 2
+                            issue = (project_relation.target).issues.find_or_create_by(subject: subject, tracker_id: tracker, author_id: user, start_date: te[:spent_on].to_date.at_beginning_of_month)
+                            te[:issue_id] = issue.id
+                        end     
+
+                        if (time_entry_relation = time_entries_relations.find_by(source_id: te[:id])).present?
+                            # Está registrada la imputación 
+                            if time_entry_relation.last_update < te[:updated_on].to_datetime
+                                # Se ha actualiza la imputación respecto a nuestro último registro => actualizamos la imputación
+                                result_update << time_entry_relation.time_entry.update_attributes(te)
+                                time_entry_relation.update_attributes({last_update: te[:updated_on], spent_on: te[:spent_on]})
+                            end
+                        else
+                            # No está registrada la imputación => creamos nueva imputación
+                            result_create << (time_entry = TimeEntry.create(te))
+                            SynchTimeEntryRelation.create({source_id: te[:id], target_id: time_entry.id, last_update: te[:updated_on], spent_on: te[:spent_on]}) if time_entry.id.present?
+                        end
+                    end
+                end
+
+                deleted_time_entries_relations = SynchTimeEntryRelation.where("spent_on BETWEEN ? AND ? AND source_id NOT IN (?)", start_date, end_date, time_entries.map{|te| te[:id]})
+                deleted_time_entries_relations.each do |te_relation|
+                    te_relation.time_entry.present? ? te_relation.time_entry.delete : te_relation.delete
+                end
+                flash[:notice] = l(:'synch_time_entries.notice_successful_synch')
+            rescue
+                flash[:error] = l(:'synch_time_entries.error_synch_failed')
+            end
+        end
+        redirect_to synch_relations_path
+    end
+
 	private
         def synch_relation_params
             params.require(:synch_relation).permit(:source_id, :source_name, :target_id, :data_type)
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ RedmineApp::Application.routes.draw do
 	resources :synch_relations
 	match '/synch_relations/get_source_subject/:id' => 'synch_relations#get_source_subject', :via => [:get, :post]
 	match '/synch_relations/get_target_subject/:id' => 'synch_relations#get_target_subject', :via => [:get, :post]
+	match '/synch_relations/null/perform_manual_synch' => 'synch_relations#perform_manual_synch', :via => [:post, :delete]
 end
